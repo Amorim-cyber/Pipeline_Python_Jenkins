18ed581fc228935a15ea336bcd3619939d3708f5
lucasa <amorim22lucas@gmail.com>
2024-08-20 16:46:47 -0300

    New adjust on conftest

--- conftest.py
+++ conftest.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 @pytest.fixture(scope="module")
 def browser():
     chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(options=chrome_options)
