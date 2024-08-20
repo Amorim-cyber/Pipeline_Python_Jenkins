f214e572954bafee371660dd1b57612c54a2e4d1
lucasa <amorim22lucas@gmail.com>
2024-08-20 16:53:11 -0300

    New adjust on conftest 2

--- conftest.py
+++ conftest.py
@@ -9,7 +9,7 @@ def browser():
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome('/var/lib/jenkins/.local/lib/python3.10/site-packages',options=chrome_options)
 
     yield driver
     driver.quit()
