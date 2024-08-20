84bf8ca176325e4fa1c1262159206c8b9050ac30
lucasa <amorim22lucas@gmail.com>
2024-08-20 17:02:23 -0300

    New adjust on conftest 3

--- conftest.py
+++ conftest.py
@@ -4,12 +4,13 @@ from selenium import webdriver
 
 @pytest.fixture(scope="module")
 def browser():
+    chrome_service = webdriver.ChromeService(executable_path='/var/lib/jenkins/.local/lib/python3.10/site-packages')
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome('/var/lib/jenkins/.local/lib/python3.10/site-packages',options=chrome_options)
+    driver = webdriver.Chrome(service=chrome_service,options=chrome_options)
 
     yield driver
     driver.quit()
